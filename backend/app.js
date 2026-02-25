ab7bf9a0c40e21906867c0d5f079806a5a3edfb9
isath2003 <isathoffl1@gmail.com>
2026-02-25 19:59:01 +0530

    Fixed

--- backend/app.js
+++ backend/app.js
@@ -8,17 +8,15 @@ const productRoutes = require('./routes/productRoutes')
 
 const app = express()
 
+
+
 app.use(morgan('dev'))
+
 app.use(express.json())
 app.use(cors())
-
 connectDB()
 
 app.use('/products', productRoutes)
 
-// ✅ IMPORTANT FIX
-const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.listen(3000)
